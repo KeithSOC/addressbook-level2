fb0739f0f8aa3c3ae3b7aae965ce6094a1366abc
Keith <october_arthur@hotmail.com>
2017-09-13 10:50:44 +0800

    Revert "Add Sort Feature"

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -2,7 +2,6 @@ package seedu.addressbook.commands;
 
 import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.AddressBook;
-import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 import java.util.List;
@@ -28,17 +27,13 @@ public class Command {
     }
 
     /**
-     * Constructs a feedback message to summarize an operation that displays a listing of persons.
+     * Constructs a feedback message to summarise an operation that displayed a listing of persons.
      *
      * @param personsDisplayed used to generate summary
      * @return summary message for persons displayed
      */
     public static String getMessageForPersonListShownSummary(List<? extends ReadOnlyPerson> personsDisplayed) {
         return String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, personsDisplayed.size());
-    }
-    
-    public static String getMessageForPersonSortedListShownSummary(List<? extends Person> personsDisplayed) {
-        return String.format(Messages.MESSAGE_PERSONS_SORTED_OVERVIEW, personsDisplayed.size());
     }
 
     /**
